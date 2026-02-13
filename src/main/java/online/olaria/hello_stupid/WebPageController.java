b2159a07c5a5f884dd25f51c26c3e2a6afaad10e
lucas-marianno <lucas.marianno94@gmail.com>
2026-02-13 17:36:34 -0300

    add endpoint 'GET' '/' with 'index.html'

---
+++ src/main/java/online/olaria/hello_stupid/WebPageController.java
@@ -0,0 +1,13 @@
+package online.olaria.hello_stupid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebPageController {
+
+  @RequestMapping("/")
+  public String home(){
+    return "index.html";
+  }
+}
